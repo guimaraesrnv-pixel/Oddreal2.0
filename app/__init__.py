2f3c197927284e92996cbf3abd7fb1e564da5e5a
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-21 20:14:05 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,25 @@
+"""
+OddReal 2.0
 
+Pacote:
+app
+
+Interface principal do sistema.
+
+Responsável por:
+- Aplicação Streamlit
+- Dashboard
+- Componentes visuais
+
+Versão: 2.0
+"""
+
+
+__version__ = "2.0"
+
+
+__author__ = "OddReal"
+
+
+
+__all__ = []
