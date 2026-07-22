4495fc7abdcfd9361eab2b066e79d9b26b0e7e62
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:10:25 -0300

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,14 @@
+from datetime import datetime
 
+
+class Helpers:
+
+    @staticmethod
+    def now():
+
+        return datetime.now()
+
+    @staticmethod
+    def timestamp():
+
+        return datetime.now().isoformat()
