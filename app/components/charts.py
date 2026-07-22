7c770171817daf240f9692d42bf8240fde759e4f
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:06:06 -0300

    Update charts.py

--- app/components/charts.py
+++ app/components/charts.py
@@ -1,1 +1,17 @@
 
+import streamlit as st
+import pandas as pd
+
+
+class Charts:
+
+    @staticmethod
+    def bar_chart(data):
+
+        if not data:
+
+            return
+
+        df = pd.DataFrame(data)
+
+        st.bar_chart(df)
