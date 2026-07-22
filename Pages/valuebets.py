bb4d30432abb20dfd2a863cf24e7062842681722
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:00:38 -0300

    Update valuebets.py

--- Pages/valuebets.py
+++ Pages/valuebets.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+
+
+def render(opportunities=None):
+    """
+    Página de Value Bets.
+    """
+
+    st.title("💰 Value Bets")
+
+    if not opportunities:
+        st.warning("Nenhuma oportunidade encontrada.")
+        return
+
+    for value in opportunities:
+
+        st.write(value)
