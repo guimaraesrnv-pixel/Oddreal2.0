09f7c37c012f701a2098e4ce3ee9053ab3cdf1ba
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:04:03 -0300

    Update __init__.py

--- app/components/__init__.py
+++ app/components/__init__.py
@@ -1,1 +1,16 @@
 
+"""
+OddReal 2.0
+
+Pacote de componentes visuais.
+"""
+
+from .cards import Cards
+from .tables import Tables
+from .charts import Charts
+
+__all__ = [
+    "Cards",
+    "Tables",
+    "Charts"
+]
