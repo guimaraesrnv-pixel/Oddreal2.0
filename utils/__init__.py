d67cd76dc972377d1fcd856b8b0f7759bbe689d2
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:08:45 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,16 @@
 
+"""
+OddReal 2.0
+
+Pacote de utilitários.
+"""
+
+from .logger import Logger
+from .helpers import Helpers
+from .security import Security
+
+__all__ = [
+    "Logger",
+    "Helpers",
+    "Security"
+]
