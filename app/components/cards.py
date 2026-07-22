14c0b25a224bac6ef3e43eacb1967cc20e4ad693
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:04:43 -0300

    Update cards.py

--- app/components/cards.py
+++ app/components/cards.py
@@ -1,1 +1,22 @@
+import streamlit as st
 
+
+class Cards:
+    """
+    Componentes em formato de cartões.
+    """
+
+    @staticmethod
+    def event_card(event):
+
+        st.container()
+
+        st.markdown(
+            f"""
+            ### {event.get('home_team')} x {event.get('away_team')}
+
+            **Liga:** {event.get('league')}
+
+            **Esporte:** {event.get('sport')}
+            """
+        )
