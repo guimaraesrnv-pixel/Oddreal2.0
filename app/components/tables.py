b9b3b656604fc1503fa9558330a095c19b3583a1
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:05:23 -0300

    Update tables.py

--- app/components/tables.py
+++ app/components/tables.py
@@ -1,1 +1,17 @@
 
+import streamlit as st
+import pandas as pd
+
+
+class Tables:
+
+    @staticmethod
+    def dataframe(data):
+
+        if not data:
+
+            st.info("Nenhum dado disponível.")
+
+            return
+
+        st.dataframe(pd.DataFrame(data))
