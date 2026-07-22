718205ce7f0ba45780890c13807b2c6ba5fff73f
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-22 17:01:42 -0300

    Update settings.py

--- Pages/settings.py
+++ Pages/settings.py
@@ -1,1 +1,16 @@
 
+import streamlit as st
+
+
+def render():
+    """
+    Página de configurações.
+    """
+
+    st.title("⚙️ Configurações")
+
+    st.write("Configurações do sistema OddReal 2.0")
+
+    st.checkbox("Atualização automática", value=True)
+
+    st.checkbox("Modo escuro", value=False)
