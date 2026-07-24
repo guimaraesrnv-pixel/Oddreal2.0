85a5c9961d302173a64905cc5621f78368d95051
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-07-24 00:09:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import sys
+import os
+
+# Força o Python a enxergar as pastas do projeto na raiz
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from app.main import main
 
 if __name__ == "__main__":
     main()
-    
