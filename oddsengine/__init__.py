c21ac8284a9b7edac9c62f7ad85da08512e0a37f
guimaraesrnv-pixel <guimaraes.rnv@gmail.com>
2026-08-05 11:41:11 -0300

    Update __init__.py

--- oddsengine/__init__.py
+++ oddsengine/__init__.py
@@ -4,17 +4,18 @@ Odds Engine
 
 Pacote responsável pelos motores matemáticos
 e estatísticos relacionados às odds.
-
-Os motores são independentes da interface,
-da API e da IA.
 """
 
 from .value import (
     ValueBetEngine,
     valuebet_engine,
+    ValueEngine,
+    value_engine,
 )
 
 __all__ = [
     "ValueBetEngine",
     "valuebet_engine",
+    "ValueEngine",
+    "value_engine",
 ]
